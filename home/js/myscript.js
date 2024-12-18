e49b2e2aeb2765bd08ab5df540cba6637eff13ab
Kelly Dumbrique <ksdumbrique2@student.apc.edu.ph>
2024-12-18 23:35:26 +0800

    js

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+  const element = document.getElementsByTagName("p");
+  document.getElementById("demo").innerHTML = element[0].innerHTML;
+}  
